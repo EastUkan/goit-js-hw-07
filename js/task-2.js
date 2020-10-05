9b6ee18e496d9f3fee4822f685867b46d06f265c
Andrii Kryvenko <andkryvenko@gmail.com>
2020-10-05 21:26:17 +0300

    добавляет структуру дз

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const listCreateEl = ingredients.map(ingredient => {
+  const ingredientEL = document.createElement('li');
+  ingredientEL.textContent = `${ingredient}`;
+  return ingredientEL;
+});
+
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...listCreateEl);
+
+console.log(ingredientsList);
---
+++ js/task-5.js
@@ -0,0 +1,12 @@
+const refs = {
+  input: document.querySelector('#name-input'),
+  span: document.querySelector('#name-output'),
+};
+
+function onInputChange(event) {
+  event.currentTarget.value !== ``
+    ? (refs.span.textContent = event.currentTarget.value)
+    : (refs.span.textContent = `незнакомец`);
+}
+
+refs.input.addEventListener('input', onInputChange);
